ef70abb4636d59cfa71621bc4dd97ae049075e19
Richard Wilson <richard@freerunningtech.com>
2016-04-20 11:10:31 -0700

    Fix user spec testing non-existant behavior
    c4afc817ebb4f1c2ebdc7726fffeddfee08776b5 removed the after_save callback
    on users, so the after save here wouldn't pass. Changed the tests for
    after_create functionality instead.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,28 @@
 require 'spec_helper'
 
 describe Spree::User do
-  context "syncing with mail chimp" do
+  context "after create" do
+    let(:subscription_double) { double Spree::Chimpy::Subscription }
+    subject { FactoryGirl.create(:user_with_subscribe_option) }
+
+    it "subscribes the user when the user is created" do
+      Spree::User.any_instance.
+        should_receive(:subscription).
+        once.
+        and_return(subscription_double)
+      expect(subscription_double).to receive(:subscribe).once
+
+      subject
+    end
+  end
+
+  context "after destroy" do
     let(:subscription) { double(:subscription, needs_update?: true) }
 
     before do
       subscription.should_receive(:subscribe)
       Spree::Chimpy::Subscription.should_receive(:new).at_least(1).and_return(subscription)
       @user = create(:user_with_subscribe_option)
-    end
-
-    it "submits after saving" do
-      subscription.should_receive(:resubscribe)
-      @user.save
     end
 
     it "submits after destroy" do
